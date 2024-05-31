594406d7b9191b29c97a1469113957ea9a07d664
D4RK08 <110988096+D4RK08@users.noreply.github.com>
2024-05-31 08:36:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackQueryHandler
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.error import BadRequest
+from telegram import Bot
 
 TOKEN = "7195518842:AAHFDbmhEfIK6PTiVuxnezlMmTa-l7n8ttM"
 
